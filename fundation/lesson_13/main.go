6280797f8455e36d8f7bc9336205662125fd4b81
Valter Roberto Garcia Junior <junior.opto@gmail.com>
2023-03-27 07:26:51 -0300

    lesson 13 done

---
+++ fundation/lesson_13/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
+
+//criando métodos para as structs
+
+func (c Cliente) Desativar() {
+	c.Ativo = false
+	fmt.Printf("O cliente %s foi desativado, ativo: %t", c.Nome, c.Ativo)
+
+}
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+	Endereco
+}
+
+func main() {
+	wesley := Cliente{
+		Nome:  "Wesley",
+		Idade: 30,
+		Ativo: true,
+	}
+	//executando o método
+	wesley.Desativar()
+
+}
