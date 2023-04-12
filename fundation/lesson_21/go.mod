54ca042c3b96768502db78900a898f62edad877f
Valter Roberto Garcia Junior <junior.opto@gmail.com>
2023-04-12 07:31:42 -0300

    lesson 21 done

---
+++ fundation/lesson_21/go.mod
@@ -0,0 +1,3 @@
+module go/expert/lesson21
+
+go 1.20
---
+++ fundation/lesson_21/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"go/expert/lesson21/matematica"
+)
+
+func main() {
+	soma := matematica.Soma(10, 20)
+	fmt.Println("O resultado é :", soma)
+}
