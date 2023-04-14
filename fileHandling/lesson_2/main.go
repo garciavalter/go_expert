9a97603a6025bd40ef42fb9e76f5b67e93150f56
Valter Roberto Garcia Junior <junior.opto@gmail.com>
2023-04-14 07:24:47 -0300

    lesson 2 done

---
+++ fileHandling/lesson_2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	req, err := http.Get("https://www.google.com")
+	if err != nil {
+		panic(err)
+	}
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(res))
+	//todo stream de dados precisa ser fechado para evitar
+	//vazamento de recursos no go
+	req.Body.Close()
+
+}
