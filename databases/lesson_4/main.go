48fff572e3ece5855b4d74fedbd63f926abbff88
Valter Roberto Garcia Junior <junior.opto@gmail.com>
2023-05-05 08:02:35 -0300

    lesson 4 done

---
+++ databases/lesson_4/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(172.31.176.1:3306)/test")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	product := NewProduct("Notebook", 1899.99)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+	product.Price = 100.0
+	err = UpdateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+	p, err := SelectOneProduct(db, product.ID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("O produto: %s possui o preço de %.2f", p.Name, p.Price)
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?, ?, ?)")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func UpdateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func SelectOneProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("select id, name, price from products where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	var p Product
+	//é possível usar querys com contexto usando stmt.QueryRowWithContext()
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return nil, err
+	}
+	return &p, nil
+
+}
