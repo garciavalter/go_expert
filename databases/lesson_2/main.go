144a78dcd828c52c7d8f551760255c183e0962f5
Valter Roberto Garcia Junior <junior.opto@gmail.com>
2023-05-05 07:36:13 -0300

    lesson 2 done

---
+++ databases/lesson_2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(172.31.176.1:3306)/test")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	product := NewProduct("Notebook", 1899.99)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?, ?, ?)")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
